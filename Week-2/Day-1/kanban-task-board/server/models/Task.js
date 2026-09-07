c3a150876cc16b904891a86627b98d1b1c952c2f
Rida Khan <199349979+nahkadir@users.noreply.github.com>
2026-09-07 15:50:20 +0500

    User & Task Model added

---
+++ Week-2/Day-1/kanban-task-board/server/models/Task.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
