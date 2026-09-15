c6e2874a57d7f8328dbdf35aade83e3b84adf7d8
Rida Khan <199349979+nahkadir@users.noreply.github.com>
2026-09-15 17:45:16 +0500

    Fixed cors bug

--- Week-3/chat-application/backend/src/server.js
+++ Week-3/chat-application/backend/src/server.js
@@ -21,7 +21,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "chat-app-frontend-flame.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-frontend-flame.vercel.app",
+    ],
   }),
 );
 app.use(express.json());
